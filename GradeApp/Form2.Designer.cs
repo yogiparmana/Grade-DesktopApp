3213322997d7578e554fed0f310fc1d01ed110c4
yogiparmana <yogiparmanaiwayan1@gmail.com>
2021-03-16 21:12:18 +0800

    Add project files.

---
+++ GradeApp/Form2.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace GradeApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textTugas = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSimpan = new System.Windows.Forms.Button();
+            this.textKuis = new System.Windows.Forms.TextBox();
+            this.textUts = new System.Windows.Forms.TextBox();
+            this.textUas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textTugas
+            // 
+            this.textTugas.Location = new System.Drawing.Point(86, 48);
+            this.textTugas.Name = "textTugas";
+            this.textTugas.Size = new System.Drawing.Size(204, 20);
+            this.textTugas.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama Mahasiswa : ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnSimpan
+            // 
+            this.btnSimpan.Location = new System.Drawing.Point(128, 152);
+            this.btnSimpan.Name = "btnSimpan";
+            this.btnSimpan.Size = new System.Drawing.Size(127, 23);
+            this.btnSimpan.TabIndex = 3;
+            this.btnSimpan.Text = "Simpan";
+            this.btnSimpan.UseVisualStyleBackColor = true;
+            this.btnSimpan.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textKuis
+            // 
+            this.textKuis.Location = new System.Drawing.Point(86, 74);
+            this.textKuis.Name = "textKuis";
+            this.textKuis.Size = new System.Drawing.Size(204, 20);
+            this.textKuis.TabIndex = 4;
+            // 
+            // textUts
+            // 
+            this.textUts.Location = new System.Drawing.Point(86, 100);
+            this.textUts.Name = "textUts";
+            this.textUts.Size = new System.Drawing.Size(204, 20);
+            this.textUts.TabIndex = 5;
+            // 
+            // textUas
+            // 
+            this.textUas.Location = new System.Drawing.Point(86, 126);
+            this.textUas.Name = "textUas";
+            this.textUas.Size = new System.Drawing.Size(204, 20);
+            this.textUas.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Tugas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Kuis";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Uts";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Uas";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 334);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textUas);
+            this.Controls.Add(this.textUts);
+            this.Controls.Add(this.textKuis);
+            this.Controls.Add(this.btnSimpan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textTugas);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GradeApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textTugas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSimpan;
+        private System.Windows.Forms.TextBox textKuis;
+        private System.Windows.Forms.TextBox textUts;
+        private System.Windows.Forms.TextBox textUas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ GradeApp/Form2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeApp
+{
+    public partial class Form2 : Form
+    {
+        string nama;
+        double checkNum = -1;
+        public Form2(string nama)
+        {
+            InitializeComponent();
+            label2.Text += nama;
+            this.nama = nama;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkNumber(textTugas.Text.ToString(),checkNum,label1) != 0 && checkNumber(textKuis.Text.ToString(), checkNum,label3) != 0 && checkNumber(textUts.Text.ToString(), checkNum,label4) != 0 && checkNumber(textUas.Text.ToString(), checkNum,label5) != 0)
+            {
+                string nHuruf;
+                double dTugas = double.Parse(textTugas.Text), dKuis = double.Parse(textKuis.Text), dUts = double.Parse(textUts.Text), dUas = double.Parse(textUas.Text), nAkhir;
+
+                nAkhir = (0.2 * dTugas) + (0.2 * dKuis) + (0.3 * dUts) + (0.3 * dUas);
+                if (nAkhir >= 85 && nAkhir <= 100)
+                    nHuruf = "A";
+                else if (nAkhir >= 65 && nAkhir < 85)
+                    nHuruf = "B";
+                else if (nAkhir >= 55 && nAkhir < 65)
+                    nHuruf = "C";
+                else if (nAkhir >= 45 && nAkhir < 55)
+                    nHuruf = "D";
+                else if (nAkhir >= 0 && nAkhir <= 45)
+                    nHuruf = "E";
+                else
+                    nHuruf = "INVALID";
+                MessageBox.Show("Nama Mahasiswa \t: "+nama+"\nNilai Akhir\t: " + nAkhir +"\nGrade\t\t: " + nHuruf);
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+
+        static double checkNumber(string str, double checkNum, Label l)
+        {
+
+            
+            double newNum = 0;
+            if (double.TryParse(str, out checkNum))
+            {
+                newNum = double.Parse(str);
+                if (newNum > 100 || newNum < 0)
+                {  
+                    MessageBox.Show("error di Form " + l.Text + ", 'Input HArus > 0 atau < 100!'");
+                    newNum = 0;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("error di Form " +l.Text+", 'Input Harus Angka!'");
+            }
+            return newNum;
+        }
+    }
+}
